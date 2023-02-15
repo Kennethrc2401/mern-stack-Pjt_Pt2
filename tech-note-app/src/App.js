287893e6b3ae74f28ed099b99e1ca74f96cf419a
Kenneth Castro <kennethramirezcastro@gmail.com>
2023-02-14 23:12:20 -0500

    Implemented Authentication
    JWT Auth, Login Auth, Route Protection, Loading Spanner, features.

--- tech-note-app/src/App.js
+++ tech-note-app/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from './features/auth/Login'
+import Login from './features/auth/Login';
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
 import NotesList from './features/notes/NotesList'
@@ -11,41 +11,66 @@ import NewUserForm from './features/users/NewUserForm'
 import EditNote from './features/notes/EditNote'
 import NewNote from './features/notes/NewNote'
 import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './config/roles'
+import useTitle from './hooks/useTitle';
 
 function App() {
+  useTitle('Dan D. Repairs')
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
+        {/* Protected Routes */}
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
 
-            <Route index element={<Welcome />} />
+                <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=':id' element={<EditUser />} />
-              <Route path='new' element={<NewUserForm />} />
+                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
+
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
+
+              </Route>{/* End Dash */}
             </Route>
-
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=':id' element={<EditNote />} />
-              <Route path='new' element={<NewNote />} />
-            </Route>
-
-
-          </Route>{/* End Dash */}
-        </Route>
+          </Route>
+        </Route>{/* End Protected Routes */}
 
       </Route>
-    </Routes>
+    </Routes >
   );
 }
 
 export default App;
 
-// Left off at: https://youtu.be/CvCiNeLnZ00?t=16624
-// https://github.com/gitdagray/mern_stack_course/blob/main/lesson_08-backend/middleware/verifyJWT.js
+// Left off at: https://youtu.be/CvCiNeLnZ00?t=27021
+// https://github.com/gitdagray/mern_stack_course
+
+// Sample User - Hank - Employee
+// Username: Hank
+// Password: !Hb12345
+
+// Sample User - Tom - Admin
+// Username: Tom
+// Password: 1234567890
+
+// Sample User - Jeff - Employee, Manager, Admin
+// Username: Jeff
+// Password: !jeff12345
